446a94e44c9c4cd2264cd8cad4e580a5b902635f
P4bl1t0ck <130107066+P4bl1t0ck@users.noreply.github.com>
2023-07-03 22:16:46 -0500

    Update Practica_18_24.c
    Yeah i update,the last one suck´s.

--- Practica_18_24.c
+++ Practica_18_24.c
@@ -1,66 +1,65 @@
 /*----------------Practica 18_24---------------------*/
 /*Funciones*/
-
+/*Desarrolo de programas lineal*/
+/*Porgramacion estructurado : busca crear || ,
+descomponer un problema complejo en pequeños problemas */
+/*Descomponer en funciones*/
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-/*Prototipo las funciones o como una declaracion de estas */
-
-void suma(int num1,int num2); 
-int sumas(int num1,int num2,int *resulta);
-
-int main (int argc, char *argv[]) {
-    /*Funciones*/
-    /*Programacion estructurada en descomposicion de funciones,en el separar una funcion donde resolvera un problema*/
-    /*Invoco a la funcion*/
-     int num1,num2;
-    printf("Introduce un numero 1\n");
-    scanf("%d",&num1);
-    printf("Intorduce el numero 2\n");
-    scanf("%d",&num2);
-    int r=num1+num2;
-    printf("La suma es de : %d\n",r);
-    suma(num1,num2);
-    int result =sumas(num1,num2,&resulta);
-    printf("La suma es : &d\n",result);
-    int resulta;
-
-    return 0;
-}
-
-/*Debe devolver algo si no esta sera void*/
-void suma(int num1 ,int num2,){
-    /*Aki hize una funcion para resolver el problema y una suma */
-    int num1,num2;
-    printf("Introduce un numero 1\n");
-    scanf("%d",&num1);
-    printf("Intorduce el numero 2\n");
-    scanf("%d",&num2);
-    int r=num1+num2;
-    printf("La suma es de : %d\n",r);
-}
-int sumas(int num1,int num2,int *resulta ){
-    int result=num1+num2;
-    return result;
-}
-
-
-void multiplicaVector(int *vector,int nElem,int numero);
+/*Prototipamos*/
+//void suma(int num1,int num2);
+//int suma(int num1,int num2);
+/*void suma(int num1,int num2,int *result);
+//------------------------------
+/*Crear un valor de variables y poder psas un parametro pro referencia*/
 int main(){
-/*Escribir una funcion que reciba como parametros un vector de enteros(por referencia) y la longitud del vector y un numero (por valor)
- la funcion debe multiplicar cada elemento del vector por el numero.*/
-    int vector[10]={1,2,3,4,5,6,7,8,9,10};
-    multiplicaVector(&vector[0],10,3);
-    for (size_t i = 0; i < 10; i++)
+    int num1,num2;
+    printf("Introduce un numero 1: \n");
+    scanf("%d",&num1);
+    printf("Introduce un numero 2: \n");
+    scanf("%d",&num2);
+    /*Invocamos*/
+    int result;/*Creamos una variable y 
+    luego pasamos por referencia en la funcion suma*/
+    /*Esta le asignamos como parametro la ubicacion 
+    de memoria de result que se declaro en la main
+    y esta al estar dentro de la funcion void pasa como refencia
+    y dentro de la funcion modificamos el espacio de memoria al que apunta el puntero ala 
+    y hemos moficamor el valor de en la funcion y la imprimimos en la funcion main.*/
+    suma(num1,num2,&result);
+    printf("La suma de sus 2 numeros es :%d\n",result);
+}
+/*Cuando la funcion no devuelve nada y no recibe nada*/
+void suma(int num1,int num2,int *result){
+    *result = num1+num2;
+}
+//----------------------------------------------------------------------------------------------//
+/*Multiplicar Vector por un valor*/
+/*Practica 18-24*/
+/*Escribir una funcion que reciba como parametros un vector de enteros , 
+(por referencia) y la longitud del vector y un numero(por valor)
+La funcion debe multiplicar cada elemento del vector por el numero.*/
+
+void mutiplica_vector(int *vector,int nElem,int numero);
+
+int main()
+{
+int vector[10] = {1,2,3,4,5,6,7,8,9,10};
+multiplica_vector(&vector[0],10,3);
+for (size_t i = 0; i < 10; i++)
+{
+    printf("%d",vector[i]);
+}
+
+
+}
+void multiplica_vector(int *vector,int nElem,int numero){
+    for (size_t i = 0; i < nElem; i++)
     {
-        printf("%d ",vector[i]);
+        *(vector+i) = *(vector+i) * numero;
     }
     
- return 0;
-}
-void multiplicaVector(int *vector,int nElem,int numero){
-for (size_t i = 0; i < nElem; i++){
-    *(vector+i) = *(vector+i) * numero;
-}
+
 }
